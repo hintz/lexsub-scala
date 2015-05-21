7c059c558b1184820e6ff8678c556155da0d1053
Gerold <gerold.hintz@gmail.com>
2015-05-21 15:41:58 +0200

    init of empty project

---
+++ src/main/scala/de/tudarmstadt/langtech/lexsub_scala/App.scala
@@ -0,0 +1,12 @@
+package de.tudarmstadt.langtech.lexsub_scala
+
+object App {
+  
+  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  
+  def main(args : Array[String]) {
+    println( "Hello World!" )
+    println("concat arguments = " + foo(args))
+  }
+
+}
