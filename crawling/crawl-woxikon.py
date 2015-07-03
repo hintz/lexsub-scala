f5730613ce60284b0dba77d1de75a85ac62b67a9
Gerold <gerold.hintz@gmail.com>
2015-07-03 15:50:43 +0200

    ported crawl-woxikon.py

--- crawling/crawl-woxikon.py
+++ crawling/crawl-woxikon.py
@@ -1,34 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import mechanize, sys, time
-from mechanize._util import write_file
+from crawler import LexsubCrawler
 
-OutFolder = 'woxikon'
-SleepSeconds = 4
+class WoxikonCrawler(LexsubCrawler):
+	def __init__(self): LexsubCrawler.__init__(self, 
+		instanceFile = '../targets.txt', 
+		outFolder = 'woxikon',
+		initUrl = "http://synonyme.woxikon.de/")
 
-InstanceFile = '../AIPHES_Data/GermEval2015/train-dataset.gold'
-instanceFile = open(InstanceFile, 'rU')
-instances = set(x.split()[0].split('.')[0] for x in instanceFile.readlines())
+	def search(self, keyword):
+		br = self.br
+		br.select_form(nr=0)
+		br['q'] = keyword
+		result = br.submit()
+		return result.read()
 
-br = mechanize.Browser()
-br.set_handle_robots(False)
-br.open("http://synonyme.woxikon.de/")
-
-def search_and_save(keyword):
-	br.select_form(nr=0)
-	br['q'] = keyword
-	result = br.submit()
-	data = result.read()
-	write_file(OutFolder + '/' + keyword + ".html", data)
-
-# search_and_save('fesselnd')
-# search_and_save('Erleichterung')
-
-for word in instances:
-	print word + '..'
-	try: 
-		search_and_save(word)
-	except Exception as e:
-		print "ERROR " + e + " for " + word
-	time.sleep(SleepSeconds)
+WoxikonCrawler().crawl()
