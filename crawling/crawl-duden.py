533bba303b9204c86afba01bf3e659a85a73ee2c
Gerold <gerold.hintz@gmail.com>
2015-07-03 15:47:08 +0200

    ported crawl-duden.py

--- crawling/crawl-duden.py
+++ crawling/crawl-duden.py
@@ -1,30 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import mechanize, sys, time, os, string
-from mechanize._util import write_file
+from crawler import LexsubCrawler
 
-OutFolder = 'duden'
-SleepSeconds = 3
+class DudenCrawler(LexsubCrawler):
+	def __init__(self): LexsubCrawler.__init__(self, 
+		instanceFile = '../targets.txt', 
+		outFolder = 'duden',
+		initUrl = "http://www.duden.de/")
 
-InstanceFile = '../targets.txt'
-instances = set(map(string.rstrip, open(InstanceFile, 'rU').readlines()))
+	def search(self, keyword):
+		br = self.br
+		br.select_form(nr=0)
+		br['s'] = keyword
+		br.submit()
+		result = br.follow_link(url_regex=r"rechtschreibung", nr=0)
+		return result.read()
 
-br = mechanize.Browser()
-br.open("http://www.duden.de/")
-
-def search_and_save(keyword):
-	br.select_form(nr=0)
-	br['s'] = keyword
-	br.submit()
-
-	result = br.follow_link(url_regex=r"rechtschreibung", nr=0)
-	data = result.read()
-	write_file(OutFolder + '/' + keyword + ".html", data)
-
-if not os.path.exists(OutFolder): os.makedirs(OutFolder)
-
-for word in instances:
-	print word + '..'
-	search_and_save(word)
-	time.sleep(SleepSeconds)
+DudenCrawler().crawl()
