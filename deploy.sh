73b264fb1194d04c8e15a9bf7dad3518309076fd
Gerold <gerold.hintz@gmail.com>
2015-07-10 18:39:32 +0200

    added run-scripts

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+mvn --version
 mvn package
 scp target/lexsub-scala-0.0.1-SNAPSHOT.jar gerold@farnsworth.lt.informatik.tu-darmstadt.de:~/lexsub-scala/target/lexsub-scala-0.0.1-SNAPSHOT.jar
 rm dependency-reduced-pom.xml
