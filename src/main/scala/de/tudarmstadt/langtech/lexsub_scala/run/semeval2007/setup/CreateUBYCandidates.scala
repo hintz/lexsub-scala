209758486db567fff4be8ecfa52c9ba964039682
Gerold <gerold.hintz@gmail.com>
2015-11-23 11:35:38 +0100

    refactored UBYUtil

--- src/main/scala/de/tudarmstadt/langtech/lexsub_scala/run/semeval2007/setup/CreateUBYCandidates.scala
+++ src/main/scala/de/tudarmstadt/langtech/lexsub_scala/run/semeval2007/setup/CreateUBYCandidates.scala
@@ -1,4 +1,4 @@
-package de.tudarmstadt.langtech.lexsub_scala.twsi.setup
+package de.tudarmstadt.langtech.lexsub_scala.run.semeval2007.setup
 
 import de.tudarmstadt.langtech.scala_utilities.{ io, strings }
 import de.tudarmstadt.ukp.lmf.transform.DBConfig
@@ -7,10 +7,10 @@ import scala.collection.JavaConverters._
 import de.tudarmstadt.ukp.lmf.model.core.Lexicon
 import de.tudarmstadt.langtech.lexsub_scala.utility.UBYUtil
 import de.tudarmstadt.langtech.lexsub_scala.utility.UBYUtil
-import de.tudarmstadt.langtech.lexsub_scala.twsi.Settings
+import de.tudarmstadt.langtech.lexsub_scala.run.semeval2007.Settings
 
 object CreateUBYCandidates extends App {
 	val ubyDatabase = "../AIPHES_Data/UBY/ubymedium070"
   val ubyUtil = new UBYUtil(ubyDatabase)
-  ubyUtil.createCandidates(Settings.targetsPosFile, "resources/twsi2/candidates")
+  ubyUtil.createCandidates(Settings.targetsPosFile, Settings.resourcesFolder + "/candidates")
 }
