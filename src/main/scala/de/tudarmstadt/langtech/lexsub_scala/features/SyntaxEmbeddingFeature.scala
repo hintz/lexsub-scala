5cb811011a8a69a90c26b7549c0ac6314d116417
Gerold <gerold.hintz@gmail.com>
2015-11-30 19:07:22 +0100

    all combination methods from Melamud et. al, 2015

--- src/main/scala/de/tudarmstadt/langtech/lexsub_scala/features/SyntaxEmbeddingFeature.scala
+++ src/main/scala/de/tudarmstadt/langtech/lexsub_scala/features/SyntaxEmbeddingFeature.scala
@@ -9,15 +9,48 @@ import org.cleartk.classifier.Feature
 
 trait SyntacticEmbeddingCombinator {
   def apply(wordCossim: Option[Double], contextCossims: List[Double]): Double
+  def pcos(cossim: Double): Double = (cossim + 1) / 2d
 }
 
 object SyntacticEmbeddingCombinator {
+  
+  case object WordSimOnly extends SyntacticEmbeddingCombinator {
+    def apply(wordCossim: Option[Double], contextCossims: List[Double]): Double = 
+      wordCossim.getOrElse(0d)
+  }
+  
+  case object ContextMeanOnly extends SyntacticEmbeddingCombinator {
+    def apply(wordCossim: Option[Double], contextCossims: List[Double]): Double =
+      compute.mean(contextCossims)
+  }
+  
   case object Add extends SyntacticEmbeddingCombinator {
     def apply(wordCossim: Option[Double], contextCossims: List[Double]): Double = 
       (wordCossim.sum + contextCossims.sum) / (contextCossims.length + 1) 
   }
+  
+  case object BalAdd extends SyntacticEmbeddingCombinator {
+    def apply(wordCossim: Option[Double], contextCossims: List[Double]): Double = {
+      val c = contextCossims.length
+      (c * wordCossim.sum + contextCossims.sum) / (2 * c)
+    }
+  }
+  
+  case object Mult extends SyntacticEmbeddingCombinator {
+    def apply(wordCossim: Option[Double], contextCossims: List[Double]): Double = {
+      val mult = (wordCossim.toList ++ contextCossims).map(pcos).product
+      Math.pow(mult, 1 / (1d + contextCossims.length))
+    }
+  }
+  
+   case object BalMult extends SyntacticEmbeddingCombinator {
+    def apply(wordCossim: Option[Double], contextCossims: List[Double]): Double = {
+      val c = contextCossims.length
+      val mult = wordCossim.map(Math.pow(_, c)).getOrElse(1d) * contextCossims.map(pcos).product
+      Math.pow(mult, 1 / (2 * c))
+    }
+  }
 }
-
 
 /** For each instance, we retrieve the embedding of the target word and all its syntax elements */
 case class SyntacticEmbedding(
@@ -39,7 +72,8 @@ extends SmartFeature[SyntacticEmbedding] with NumericFeature {
     val syntaxElements = item.sentence.edges.collect {
       case DepEdge(label, `targetTokenIdx`, to) =>
         label + "_" + item.sentence.tokens(to).lemma
-      case DepEdge(label, from, `targetTokenIdx`) =>
+      case DepEdge(label, from, `targetTokenIdx`) => 
+        // FIXME: do we really ignore the direction of edges?
         label + "_" + item.sentence.tokens(from).lemma
     }
     
