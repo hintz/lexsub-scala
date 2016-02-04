9d1c327f4b8c681cca83ae5220da9a0cf19776aa
Gerold <gerold.hintz@gmail.com>
2016-02-04 17:32:44 +0100

    improved caching in WordVectorFile

--- src/main/scala/de/tudarmstadt/langtech/lexsub_scala/filereader/WordVectorFile.scala
+++ src/main/scala/de/tudarmstadt/langtech/lexsub_scala/filereader/WordVectorFile.scala
@@ -7,22 +7,28 @@ import de.tudarmstadt.langtech.scala_utilities.io
 import de.tudarmstadt.langtech.scala_utilities.index_file.PrefixIndexedFile
 import de.tudarmstadt.langtech.scala_utilities.index_file.CachedPrefixIndexedFile
 import de.tudarmstadt.langtech.lexsub_scala.utility.LexsubUtil
+import de.tudarmstadt.langtech.scala_utilities.cache.FileBackedCache
 
 /** A simple reader for word vector files in the format [word] [SPACE] [floating-point vector].
  *  Internally uses PrefixIndexedFile which transparently builds an index */
-class WordVectorFile(val embedding_file: String)  {
+class WordVectorFile(val embedding_file: String, useCache: Boolean = true)  {
   
-  val file = new CachedPrefixIndexedFile(embedding_file, 10, cachefile = LexsubUtil.getCachefile(embedding_file))
-  
-  /** Yields similar words based on this DT */
-  def apply(word: String): Option[Vector[Double]] = repr(word)
-  val repr: String => Option[Vector[Double]] = Memo.mutableHashMapMemo { word =>
+  val file = new PrefixIndexedFile(embedding_file, 10)
+
+  val lookup: String => Option[Vector[Double]] = Memo.mutableHashMapMemo { word =>
     val lines = file.search(word)
     val v = lines.map(_.split(" ").toList).collectFirst {
       case `word` :: vector => vector.map(_.toDouble).toArray
     }
     v.map(DenseVector.apply)
   }
+  
+  val cache: String => Option[Vector[Double]] = 
+    if(useCache) FileBackedCache(lookup, filename = LexsubUtil.getCachefile(embedding_file)).apply
+    else lookup
+  
+  /** Yields similar words based on this DT */
+  def apply(word: String): Option[Vector[Double]] = cache(word)
   
   override def toString = "WordVectorFile(%s)".format(io.filenamePart(embedding_file))
 }
