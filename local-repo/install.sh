cb42a242a56d14f07579fc18daad317e591a4c6b
Gerold <gerold.hintz@gmail.com>
2016-02-14 15:01:51 +0100

    added ranklib as non-maven dependency

--- local-repo/install.sh
+++ local-repo/install.sh
@@ -1,1 +1,8 @@
-mvn deploy:deploy-file -Durl=file:///Users/gerold/AIPHES/Code/lexsub-scala/local-repo -Dfile=jmwn-1.2.jar -DgroupId=org.itc -DartifactId=org.itc.mwn -Dpackaging=jar -Dversion=1.0.0
+#!/bin/sh
+
+# multiwordnet
+mvn deploy:deploy-file -Durl=file://$(dirname $0) -Dfile=jmwn-1.2.jar -DgroupId=org.itc -DartifactId=org.itc.mwn -Dpackaging=jar -Dversion=1.0.0
+
+# ranklib
+mvn deploy:deploy-file -Durl=file://$(dirname $0) -Dfile=RankLib-2.1.jar -DgroupId=ciir.umass.edu -DartifactId=ciir.umass.edu.RankLib -Dpackaging=jar -Dversion=2.1.0
+
