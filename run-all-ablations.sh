028e7e5feab6be47eb3a7a8fdac1de384d8e26d3
Gerold <gerold.hintz@gmail.com>
2016-03-17 13:47:18 +0100

    tmp script

---
+++ run-all-ablations.sh
@@ -0,0 +1,4 @@
+./run-ablation.sh semanticRelations -onDemand >ablation-full-semanticRelations.log 2>&1 
+./run-ablation.sh frequencyFeatures -onDemand >ablation-full-frequencyFeatures.log 2>&1 
+./run-ablation.sh distributionalFeatures -onDemand >ablation-full-distributionalFeatures.log 2>&1 
+./run-ablation.sh syntacticFeatures -onDemand >ablation-full-syntacticFeatures.log 2>&1 
